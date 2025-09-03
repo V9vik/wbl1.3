6b3d953d25d5d5936cbb95ad1f17d6cc55e218e1
V9vik <myzikov03@gmail.com>
2025-09-03 19:50:13 +0300

    add main.go

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 package main
 
-import "fmt"
-
-func WriteBit(n int64, i uint, val bool) int64 {
-	if i >= 64 {
-		return n
-	}
-
-	mask := int64(1 << i)
-
-	n &^= mask
-
-	if val == true {
-		n |= mask
-	}
-
-	return n
-}
+import (
+	"fmt"
+)
 
 func main() {
-	// ожидаем 5 (0101₂): из 4 (0100₂) включаем 0-й бит
-	fmt.Println(WriteBit(4, 0, true))
+	xs := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	cha1 := make(chan int)
+	cha2 := make(chan int)
 
-	// ожидаем 1 (0001₂): из 5 (0101₂) выключаем 2-й бит
-	fmt.Println(WriteBit(5, 2, false))
+	go func() {
+		for _, x := range xs {
+			cha1 <- x
+		}
+		defer close(cha1)
+	}()
+
+	go func() {
+		for x := range cha1 {
+			cha2 <- x * 2
+		}
+		defer close(cha2)
+	}()
+
+	for v := range cha2 {
+		fmt.Println(v)
+	}
+
 }
